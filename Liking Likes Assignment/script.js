4f33d555996c518ac30ebb09f4aa51c7dafd38c4
David Lee <123603692+leecydavid@users.noreply.github.com>
2023-03-18 11:45:11 -0700

    Add files via upload

---
+++ Liking Likes Assignment/script.js
@@ -0,0 +1,62 @@
+// var count = 9;
+// var countElement = document.querySelector('#count');
+// function add1() {
+//     count++;
+//     countElement.innerText = count + 'like(s)';
+//     // console.log(count);
+// }
+
+// var count = 9;
+// // var countElement = document.querySelector('#count');
+// function add1() {
+//     count++;
+//     newCount = document.querySelector('#count');
+//     // console.log(newCount);
+//     countElement.innerText = count + 'like(s)';
+// }
+
+var count = 9;
+
+function add1() {
+    count++;
+    console.log(count)
+    document.querySelector('#count').innerText = count + 'like(s)';
+}
+
+var counti = 12;
+
+function add1more() {
+    counti++;
+    console.log(counti)
+    document.querySelector('#count1').innerText = counti + 'like(s)';
+}
+
+var county = 9;
+
+function add1moree() {
+    console.log(county)
+    county++;
+    document.querySelector('#count2').innerText = county + 'like(s)';
+}
+
+// document.getElementbyId('like').innerhtml = i + '(likes)'
+
+
+// var count1 = 12
+// let countElement = document.querySelector('#count2')
+
+// function add1() {
+//     count++;
+//     let newCount = document.querySelector('#count2');
+//     console.log(newCount);
+//     countElement.innerText = count1 + 'like(s)';
+
+// }
+
+// var count = 12;
+// var countElement = document.querySelector('#count');
+
+// function add2() {
+//     count++;
+//     countElement.innertext = count + 'like(s)';
+// }
