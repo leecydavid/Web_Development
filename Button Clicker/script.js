239c4dd368bd68917b6ce6baed6c9a3264dad46a
David Lee <123603692+leecydavid@users.noreply.github.com>
2023-03-18 11:44:21 -0700

    Add files via upload

---
+++ Button Clicker/script.js
@@ -0,0 +1,19 @@
+function turnOff(element) {
+    element.innerText = "Logout";
+}
+
+function hide(element) {
+    element.remove();
+}
+
+
+
+function showPopup () {
+    alert('Ninja was liked');
+}
+
+
+// function showPopup (msg) {
+// alert(msg);
+// }
+// html: insert msg within showPopup parenthesis (same result)
