e1bf19f9c9abcf8023ef3408de6e7fb223005309
David Lee <123603692+leecydavid@users.noreply.github.com>
2023-03-18 11:44:50 -0700

    Add files via upload

---
+++ Dojo Weather Assignment/script.js
@@ -0,0 +1,18 @@
+function showPopup () {
+    alert('Loading weather report...');
+}
+
+function showPopup1 () {
+    alert('Loading weather report...');
+}
+
+function showPopup2 () {
+    alert('Loading weather report...');
+}
+
+function hide() {
+    document.querySelector('#message').remove();
+}
+
+// dont forget parenthesis!
+
